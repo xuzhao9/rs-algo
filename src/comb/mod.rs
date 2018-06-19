559bc0d74456fa59e066122561cd5cb0f2819549
xuzhao9 <i@xuzhao.net>
2018-06-19 16:24:10 -0400

    Initial commit.

---
+++ src/comb/mod.rs
@@ -0,0 +1,7 @@
+//
+// Combination algorithms
+// Author: xuzhao9 (i@@xuzhao.net)
+//
+
+pub fn select() {
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//
+// Algorithms implemented in Rust
+// Used as a library
+//
+
+mod comb;
+
+fn main() {
+    println!("Hello, world!");
+}
