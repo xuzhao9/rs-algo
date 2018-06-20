192c1ac13f1bfcad41191982b5b6a9a3850d6f24
xuzhao9 <i@xuzhao.net>
2018-06-20 13:57:14 -0400

    Adding boyer-moore algorithm.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 //
 // Algorithms implemented in Rust
-// Used as a library
 //
 
 fn main() {
-  println!("Hello, world!");
 }
---
+++ src/string/matching.rs
@@ -0,0 +1,19 @@
+//
+// String matching algorithms
+// Author: xuzhao9 (i@xuzhao.net)
+//
+
+// If no match, return none
+// Else, return Some(index), where index is the first match
+pub fn boyer_moore(y: String, x: String) -> Option<usize> {
+  // calculate 
+}
+
+fn bm_goodsfx_shift() {
+}
+
+fn bm_badchar_shift() {
+}
+
+pub fn kmp() {
+}
