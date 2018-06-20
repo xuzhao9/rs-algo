bd449c1a1c3bd948be975a79f49f7b44369d32a7
xuzhao9 <i@xuzhao.net>
2018-06-20 10:22:20 -0400

    Initial commit.

--- src/comb/mod.rs
+++ src/comb/mod.rs
@@ -1,7 +1,30 @@
 //
-// Combination algorithms
+// Permuation and Combination
 // Author: xuzhao9 (i@@xuzhao.net)
 //
 
-pub fn select() {
+use std::collections::HashSet;
+
+pub fn permutation(m: i32, n: i32) -> Option<Vec<HashSet<i32>>> {
+  if n == 0 { return None; }
+  Some(Vec::new())
+}
+
+// Select a group of n elements from 1..m
+pub fn combination(m: Vec<i32>, n: i32) -> Option<Vec<HashSet<i32>>> {
+  if n <= 0 {
+    return None;
+  }
+  let mut results: Vec<HashSet<i32>> = Vec::new();
+  for e in m {
+    let mut h: HashSet<i32> = HashSet::new();
+    h.insert(e);
+    results.push(h);
+  }
+  for num in 2..n {
+    // now calculate (m, 1)
+    let mut rest = num;
+    let mut cur:HashSet<i32> = HashSet::new();
+  }
+  Some(results)
 }
